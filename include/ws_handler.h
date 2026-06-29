8f8dee350782877567dee39e64299515eb7668d8
Agarvv <casluagarv@gmail.com>
2026-06-30 00:11:48 +0300

    C++ Websocket

--- include/ws_handler.h
+++ include/ws_handler.h
@@ -1,2 +1,10 @@
 
-int ws_handler(struct hnd_context *handle_context, struct pbuffer_chain *buffer_chain, int len);
+extern "C" {    
+    int ws_handler(struct hnd_context *handle_context, struct pbuffer_chain *buffer_chain, int len);
+}
+
+struct ws_frame_context {
+    int opcode;
+    int len;
+    void* data;
+};
--- src/ws_handler.cpp
+++ src/ws_handler.cpp
@@ -2,15 +2,25 @@
 #include<stdio.h>
 #include<worker.h>
 #include<main.h>
-
+#include<ws_handler.h>
 
 extern "C" {
     
 int ws_handler(struct hnd_context *handle_context, struct pbuffer_chain *buffer_chain, int len) {
-
-
+     
+     
     unsigned char* ptr = static_cast<unsigned char*>(buffer_chain->head->p);
     
+    bool mask = (ptr[1] & 0x80);
+     if(!mask) {
+         std::cout << "No Mask From Client. Closing Connection." << std::endl;
+     }
+    
+    std::unordered_map<int, ws_frame_context> contexts; 
+     
+     struct ws_frame_context context; 
+    
+    /* 
     unsigned char tb = ptr[1]; 
     bool mask = (tb & 0x80);
     int l = (tb & 0x7F); 
@@ -45,7 +55,8 @@ int ws_handler(struct hnd_context *handle_context, struct pbuffer_chain *buffer_
     
     printf("Final Len: %d\n", l);
     
-    unsigned char by = ptr[0];
+    */ 
+    
     // 0000 0000 
     // ^ FIN 
     
@@ -65,15 +76,48 @@ int ws_handler(struct hnd_context *handle_context, struct pbuffer_chain *buffer_
     
     // 67 67 67 67 67 67 67 67 67 67
     
-    switch(by & 0x0F) {
+    int opcode = (ptr[0] & 0x0F);
+    context.opcode = opcode; 
+    
+    
+    int length = 0; 
+    int flen = ptr[1] & 0x7F;
+    unsigned char* data; 
+    
+    if(flen == 126) {
+        length = flen + ptr[2] + ptr[3]; 
+        std::cout << "Exceptional Length" << std::endl; 
+        context.len = length; 
+        *data = ptr[7]; 
+    } else if (flen == 127) {
+        length = flen + ptr[2] + ptr[3]+ ptr[4] + ptr[5] + ptr[6] + ptr[7] + ptr[8] + ptr[9]; 
+        *data = ptr[13]; 
+        context.len = length; 
+        
+        std::cout << "Exceptional Length" << std::endl; 
+    }
+    
+    std::cout << "Not Exceptional Length" << std::endl; 
+    context.len = flen; 
+    
+    *data = ptr[5]; 
+    
+    
+    switch(opcode) {
         case 0x01:
              std::cout << "TEXT" << std::endl;
+             
              break;
         default:
              std::cout << "NONE" << std::endl;
     }
-
     
+    
+    
+    // if Not FIN
+    if(!(ptr[0] & 0x80)) {
+        
+    }
     
     
     return 0;
